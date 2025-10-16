1a6ddeb308c5a1f2350872f955a08fdd3bee8c70
TrendCather <karleo@kyonggi.ac.kr>
2025-10-16 13:36:45 +0900

    [Added] Readme.md, key architect idea added

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,14 @@
 package calculator;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
+
+        line.split(".")
     }
 }
