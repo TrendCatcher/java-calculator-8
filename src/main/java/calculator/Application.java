fbce9ce722a59059f7d6c1a7e553427b8c6d4917
TrendCather <karleo@kyonggi.ac.kr>
2025-10-16 11:55:19 +0900

    [Added] Readme.md, functional requirement analysis and architect

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
     }
 }
